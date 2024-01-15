bcfe71216d6da428267e003b26392077d1d0317d
arnaud <afavre@student.42lausanne.ch>
2024-01-15 12:38:31 +0100

    module finished

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,7 +1,26 @@
-#include "../include/array.tpp"
+#include "../include/Array.tpp"
 
-int main() {
-	int *a = new int();
+int main( void )
+{
+	Array< float > test;
 
-	std::cout << *a << std::endl;
+	Array< int > intArray(10);
+	Array< int > intArray2(5);
+
+	for (unsigned int i = 0; i < intArray.size(); i++ )
+		intArray[i] = i * 2;
+
+	std::cout << "Int Array 1: " << intArray << std::endl;
+
+	intArray2 = intArray;
+
+	std::cout << "int Array 2: " << intArray2 << std::endl;
+
+	try {
+		std::cout << "Accessing a valid index: " << intArray[5] << std::endl;
+		std::cout << "Accessing an invalid index: " << intArray[10] << std::endl;
+	} catch (Array< int >::OutOfBoundsException& e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	return (0);
 }
